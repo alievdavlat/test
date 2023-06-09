050a6cc75031613d38a5cccf89a2427abfc045d7
alievv <alievdavlat024@gmail.com>
2023-06-09 12:52:13 +0500

    add

---
+++ index.js
@@ -0,0 +1,38 @@
+// //  function logger(str, n){
+// //  return console.log(str.slice(0, n) + str.slice(n + 1, str.length));
+// // }
+
+// // logger('davlat', 2)
+
+
+// // let str = 'davlat'
+
+// // console.log(str.slice(0, 1));
+// // console.log(str.slice(-1));
+// // console.log(str.slice(1, -1));
+
+
+
+// function logger(str){
+// if (str.length === 0 || str.length === 1) {
+//     return str
+// } else{
+//   let ortasi = str.slice(1, -1)
+
+//   return console.log(str.slice( -1) + ortasi + str.slice(0, 1));
+// }
+// }
+
+// logger('davlat')
+
+// let str  = 'davlat'
+
+// console.log(str.slice(-1) + str + str.slice(-1));
+
+
+// // const num = [1,2,3,4,5,6]
+
+// // const somed = num.copyWithin(0,3)
+// // console.log(`somed`, somed);
+
+
